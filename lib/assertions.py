c2caef6ce7de0e8c848a031a4c7a493b37e7cd62
Сергей Берсенев <bersenev@mail.ru>
2025-01-12 13:05:02 +0300

    Lesson_04 в процессе

--- lib/assertions.py
+++ lib/assertions.py
@@ -11,3 +11,39 @@ class Assertions:
 
         assert name in response_as_dict, f"В ответе JSON нет ключа '{name}'"
         assert response_as_dict[name] == expected_value, error_message
+
+
+    @staticmethod
+    def assert_json_has_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Ответ не в формате JSON. Текст ответа: '{response.text}'"
+
+        assert name in response_as_dict, f"В ответе JSON нет ключа '{name}'"
+
+
+    @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Ответ не в формате JSON. Текст ответа: '{response.text}'"
+        for name in names:
+            assert name in response_as_dict, f"В ответе JSON нет ключа '{name}'"
+
+
+    @staticmethod
+    def assert_json_has_not_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Ответ не в формате JSON. Текст ответа: '{response.text}'"
+
+        assert name not in  response_as_dict, f"В ответе JSON не должно быть ключа '{name}. Но он есть'"
+
+
+    @staticmethod
+    def assert_code_status(response: Response, expected_status_code):
+        assert response.status_code == expected_status_code, \
+            f"Unexpected ststus code. Expected: {expected_status_code}. Actual: {response.status_code}"
