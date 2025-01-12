54a8ec7af11a1f1baf5f457e9ac89cfa5af34bcc
Сергей Берсенев <bersenev@mail.ru>
2025-01-12 15:14:36 +0300

    Lesson_04 в процессе

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,5 +1,6 @@
 import json.decoder
 from requests import Response
+from datetime import datetime
 
 
 class BaseCase:
@@ -7,9 +8,11 @@ class BaseCase:
         assert cookie_name in response.cookies, f"Не можем найти cookie с именем {cookie_name} в последнем ответе"
         return response.cookies[cookie_name]
 
+
     def get_header (self, response: Response, headers_name):
         assert headers_name in response.headers, f"Не можем найти header с именем {headers_name} в последнем ответе"
         return response.headers[headers_name]
+
 
     def get_json_value(self, response: Response, name):
         try:
@@ -20,3 +23,18 @@ class BaseCase:
         assert name in response_as_dict, f"Ответ JSON не имеет ключа '{name}'"
 
         return response_as_dict[name]
+
+
+    def prepare_registration_data(self, email=None):
+        if email is None:
+            base_part = "learnqa"
+            domain = "example.com"
+            random_part = datetime.now().strftime("%Y%m%d%H%M%S")
+            email = f"{base_part}{random_part}@{domain}"
+        return {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
