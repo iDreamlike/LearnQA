193cf9e218cdcb1303e76240cc0cc93f6292a164
Сергей Берсенев <bersenev@mail.ru>
2025-01-13 18:07:04 +0300

    ex16

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -2,8 +2,10 @@ import pytest
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+import allure
 
 
+@allure.epic("Authorization cases")
 class TestUserAuth(BaseCase):
     exclude_params = [
         ("no_cookies"),
@@ -21,6 +23,7 @@ class TestUserAuth(BaseCase):
         self.token = self.get_header(response1, "x-csrf-token")
         self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
+    @allure.description("This test successfully authorized user by email and password")
     def test_auth_user(self):
         response2 = MyRequests.get(
             "/user/auth",
@@ -35,6 +38,7 @@ class TestUserAuth(BaseCase):
             "User id из auth method не равен user id из check method"
         )
 
+    @allure.description("This test cheks authorization status w/o sending auth cookie or token")
     @pytest.mark.parametrize('condition', exclude_params)
     def test_negative_auth_check(self, condition):
         if condition == "no_cookies":
